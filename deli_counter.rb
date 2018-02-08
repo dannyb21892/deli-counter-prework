9d1a2e99f6cc53ec1d1b49131028e4e4d02610ae
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-08 00:42:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,7 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
-  output = "The line is currently"
-  if katz_deli.length == 0
-    output = output + " empty."
-  else
-    output = output + ":"
-    for i in (0..katz_deli.length-1)
-      output = output + " #{i+1}. #{katz_deli{i}:
-    end
-  end
-end
+
 
 output = "string"
 puts output << " testing"
